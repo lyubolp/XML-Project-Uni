d4b907c0511b3fb40e5fb26e644b12a4d0df9b96
Lyuboslav Karev <lyubo.karev@gmail.com>
2019-11-13 09:56:04 +0200

    Wrote the main methods for the XMLDocument class

--- src/XMLDocument.py
+++ src/XMLDocument.py
@@ -5,13 +5,9 @@ import xml.etree.ElementTree as ET
 '''
 
 class XMLDocument:
-    # https://stackabuse.com/reading-and-writing-xml-files-in-python/
     def __init__(self):
         self._doc = None
         self._root = None
-
-    def create(self):
-        pass
 
     def open(self, file_path: str):
         self._doc = ET.parse(file_path)
@@ -23,28 +19,24 @@ class XMLDocument:
         file = open(self._path, "w")
         file.write(data)
 
-        self.open(self._path)
-
-    def add_element(self, root: str, tag: str, content: str, attributes: dict = None) -> ET.Element:
-
+    def add_element(self, root: str, tag: str, content: str, attributes: dict = None):
         added_item = ET.SubElement(self.get_first_element(root), tag, attributes)
         added_item.text = content
-        return added_item
 
     def add_attribute(self, tag: str, key_value: (str, str)):
-        self.get_first_element(tag)
+        self.get_first_element(tag).attrib[key_value[0]] = key_value[1]
 
-    def add_content(self):
-        pass
+    def add_content(self, tag: str, content: str):
+        self.get_first_element(tag).text = content
 
-    def edit_element(self):
-        pass
+    def edit_element(self, old_tag: str, new_tag: str):
+        self.get_first_element(old_tag).tag = new_tag
 
-    def edit_attribute(self):
-        pass
+    def edit_attribute(self, tag: str, key_value: (str, str)):
+        self.get_first_element(tag).attrib[key_value[0]] = key_value[1]
 
-    def edit_content(self):
-        pass
+    def edit_content(self, tag: str, content: str):
+        self.get_first_element(tag).text = content
 
     def get_elements(self, name: str) -> list:
         return self._root.findall(".//" + name)
@@ -58,12 +50,13 @@ class XMLDocument:
     def get_content(self, tag: str) -> str:
         return self.get_first_element(tag).text
 
-    def remove_element(self):
-        pass
+    def remove_element(self, tag: str):
+        parent_of_element = self._root.find(".//" + tag + "/..")
+        parent_of_element.remove(self.get_first_element(tag))
 
-    def remove_attribute(self):
-        pass
+    def remove_attribute(self, tag: str, attribute_name: str):
+        del self.get_attributes(tag)[attribute_name]
 
-    def remove_content(self):
-        pass
+    def remove_content(self, tag: str):
+        self.get_first_element(tag).text = None
 
