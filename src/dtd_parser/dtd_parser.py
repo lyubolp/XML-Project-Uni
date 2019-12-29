e8bf8c37c784e25fcc998192a954eb0e96793078
luchev <luchevz@gmail.com>
2019-12-29 16:54:34 +0200

    Refactor dtd_parser: add comments, rename private fields

--- src/dtd_parser/dtd_parser.py
+++ src/dtd_parser/dtd_parser.py
@@ -2,99 +2,155 @@ import re
 from src.dtd_attribute.dtd_attribute import *
 
 
-def get_token_children(token: str):
+DTD_ELEMENT_TAG_BEGINNING = "<!ELEMENT"
+DTD_ATTRIBUTE_TAG_BEGINNING = "<!ATTLIST"
+
+
+def _get_token_children(token: str):
     splitter = r'[\s,)(]'
     return list(filter(None, re.split(splitter, token)[2:-1]))
 
 
-def get_token_name(token: str) -> str:
+def _get_token_name(token: str) -> str:
     splitter = r'\s+'
     return list(filter(None, re.split(splitter, token, 2)[1:-1]))[0]
 
 
-def split_on_whitespace(token: str, splits: int) -> list:
+def _split_on_whitespace(token: str, splits: int) -> list:
     splitter = r'\s+'
     return list(re.split(splitter, token, splits))
 
 
 class DTDParser:
     def __init__(self, path=None):
-        self.path = path
-        self.content = ""
+        self._path = path
+        self._content = ""
+        self._tokens = dict()
         self.attributes = dict()
         self.elements = dict()
-        self.tokens = dict()
 
-        if self.path is not None:
+        if self._path is not None:
             self.load(path)
 
-    def reset_state(self):
-        self.path = None
-        self.content = ""
+    def _reset_state(self) -> None:
+        """
+        CLear the object state in order to allow a
+        new file to be loaded
+        """
+        self._path = None
+        self._content = ""
+        self._tokens = dict()
         self.elements = dict()
         self.attributes = dict()
 
-    def set_path(self, path):
-        self.path = path
+    def load(self, path) -> None:
+        """
+        Load a file from a path and parse its content
+        :param path: path to the file to load
+        """
+        self._reset_state()
+        self._path = path
 
-    def load(self, path):
-        self.reset_state()
-        self.set_path(path)
-
-        self.read_file_content()
-        self.tokenize_content()
-        self.parse_tokens()
+        self._read_file_content()
+        self._tokenize_content()
+        self._parse_tokens()
 
         # self.debug_print_attributes()
         # self.debug_print_elements()
 
-    def debug_print_attributes(self):
+    def _debug_print_attributes(self) -> None:
+        """
+        Print debug information about the attributes parsed on stdout
+        """
         for i in self.attributes.keys():
             for k in self.attributes[i]:
                 k.print()
 
-    def debug_print_elements(self):
+    def _debug_print_elements(self) -> None:
+        """
+        Print debug information about the elements parsed on stdout
+        """
         for i in self.elements.keys():
             print("Element: " + i + ", Children: ", end="")
             print(self.elements[i])
 
-    def read_file_content(self):
-        with open(self.path, "r") as file:
-            self.content = file.read()
+    def _read_file_content(self) -> None:
+        """
+        Read the content of the file pointed by _path as a string
+        """
+        with open(self._path, "r") as file:
+            self._content = file.read()
 
-    def validate_content(self):
-        if self.content == "":
-            raise ValueError("There is no content to parse from file {}".format(self.path))
+    def _validate_content(self) -> None:
+        """
+        Validate the read content of a file to make sure it's
+        valid DTD-looking file. A DTD-looking file is a file which consists
+        of xml-tags, aka all the content is enclosed in < and >
+        e.g <a><b></a>
+        This does not check for closing tags or if the DTD tags are valid
+        """
+        if self._content == "":
+            raise ValueError("There is no content to parse from file {}".format(self._path))
 
+        "xml_file_regex matches a string consisting of xml tags"
         xml_file_regex = r'^\s*<(.*?>\s*<)*.*?>\s*$'
-        if not re.match(xml_file_regex, self.content):
-            raise ValueError("The content of file {} is invalid DTD".format(self.path))
+        if not re.match(xml_file_regex, self._content):
+            raise ValueError("The content of file {} is invalid DTD".format(self._path))
 
-    def tokenize_content(self):
-        self.validate_content()
+    def _tokenize_content(self) -> None:
+        """
+        Convert the content of the file to tokens which can then be parsed as
+        valid elements or attributes. A token is each string enclosed in
+        < and >
+        """
+        self._validate_content()
 
+        "xml_tags_regex matches every XML tag separately"
         xml_tags_regex = r'<.*?>'
-        self.tokens = re.findall(xml_tags_regex, self.content)
+        self._tokens = re.findall(xml_tags_regex, self._content)
 
-    def parse_tokens(self):
-        for token in self.tokens:
-            if token.startswith("<!ELEMENT"):
-                self.add_element(token)
-            elif token.startswith("<!ATTLIST"):
-                self.add_attribute(token)
+    def _parse_tokens(self) -> None:
+        """
+        Convert tokens (strings) into objects
+        DTD Elements are converted into a dictionary with
+            key: element name
+            value: list of names of child elements
+        DTD attributes are stored in a dictionary with
+            key: element name, which this attribute belongs to
+            value: list of DTDAttribute objects representing the parsed attributes
+        """
+        for token in self._tokens:
+            if token.startswith(DTD_ELEMENT_TAG_BEGINNING):
+                self._add_element(token)
+            elif token.startswith(DTD_ATTRIBUTE_TAG_BEGINNING):
+                self._add_attribute(token)
+            else:
+                raise ValueError("The content of file {} is invalid. Invalid token found: {}".format(self._path, token))
 
-    def add_element(self, token):
-        name = get_token_name(token)
-        children = get_token_children(token)
+    def _add_element(self, token: str) -> None:
+        """
+        Parse a string, which is a DTD element, into a key-value pair
+            key: element name
+            value: child elements of this element
+        :param token: string representing DTD element, e.g. <!ELEMENT ...>
+        """
+        name = _get_token_name(token)
+        children = _get_token_children(token)
         self.elements[name] = children
 
-    def add_attribute(self, token):
+    def _add_attribute(self, token: str) -> None:
+        """
+        Parse a string, which is a DTD attribute, into a key-value pair
+            key: element name, which this attribute belongs to
+            value: DTDAttribute object (the parsed attribute)
+        :param token: string representing a DTD attribute, e.g. <!ATTLIST ...>
+        """
         attribute = DTDAttribute()
 
-        split_element_name = split_on_whitespace(token, 2)[1:]
+        split_element_name = _split_on_whitespace(token, 2)[1:]
         element_name = split_element_name[0]
         attribute.set_element_name(element_name)
-        split_attribute_name = split_on_whitespace(split_element_name[1], 1)
+        split_attribute_name = _split_on_whitespace(split_element_name[1], 1)
         attribute.set_name(split_attribute_name[0])
         token_after_attribute_name = split_attribute_name[1]
 
@@ -107,7 +163,7 @@ class DTDParser:
             attribute.set_enumerated_values([value.strip('" ') for value in split_all[:-1]])
         else:
             # Non-enumerated attribute
-            split_type = split_on_whitespace(token_after_attribute_name, 1)
+            split_type = _split_on_whitespace(token_after_attribute_name, 1)
             attribute.set_type(split_type[0])
             token_after_type = split_type[1]
             if token_after_type.startswith('#FIXED'):
