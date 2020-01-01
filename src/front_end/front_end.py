e8be7ea8396cebf30fc33074c6a256c50a7ec923
Lyuboslav Karev <lyubo.karev@gmail.com>
2020-01-01 12:12:05 +0200

    Added Wiki-api integration

--- src/front_end/front_end.py
+++ src/front_end/front_end.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template
 from .file_upload_form import FileUploadForm
 from config import Config
+from src.wiki_api.wiki_api import WikiAPI, Content
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+
+def get_content(title: str) -> Content:
+    wiki_getter = WikiAPI()
+    return wiki_getter.get_page_header_text(title)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -11,8 +17,8 @@ app.config.from_object(Config)
 def index():
     file_upload_form = FileUploadForm()
     if file_upload_form.validate_on_submit():
-        f = file_upload_form.dtd.data
-        print(f.read())
-        print(file_upload_form.wiki_article_name.data)
+        file_handle = file_upload_form.dtd.data
+        print(file_handle.read())
+        wiki_content = get_content(file_upload_form.wiki_article_name.data)
 
     return render_template('index.html', project_name='XML Project', form=file_upload_form)
